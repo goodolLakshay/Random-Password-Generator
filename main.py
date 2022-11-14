f436ade80ba186c0e71ea9f4753d16305b7cf172
Lakshay <lakshaychoudhary77712@gmail.com>
2022-11-14 14:51:44 +0530

    added code to satisfy Requirement 1.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 password_length = int(input("Please enter the length of the password required: "))
 print("_____________________________________________________")
 
+
 def rand_pass_gen(n):
     empty_string = ""
     for i in range(1, n + 1):
@@ -14,4 +15,14 @@ def rand_pass_gen(n):
     print("\n\n", "The generated password is:   ", empty_string)
 
 
-rand_pass_gen(password_length)
+if password_length < 11:
+    print("For your own safety",
+          "The suggested length is >= 12")
+    confirmation = input("Do you still wish to continue? y/n:  ")
+    if confirmation == "y":
+        rand_pass_gen(password_length)
+    else:
+        print("Thank you for trying out the Random-Password-Generator")
+        exit()
+else:
+    rand_pass_gen(password_length)
