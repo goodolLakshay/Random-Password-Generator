31e3b83c7c5db6104c553879b3158e2eceed78eb
Lakshay <lakshaychoudhary77712@gmail.com>
2022-11-14 14:38:39 +0530

    applied the use to a function, more formatting

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import random
-n=int(input("Please enter the length of the password required: "))
+
+var_length = int(input("Please enter the length of the password required: "))
 print("_____________________________________________________")
 
-def rand_pass_gen:  
-    s=""
-    for i in range(1,n+1):
-        a=chr(random.randint(65,90))
-        b=chr(random.randint(97,122))
-        c=str(int(random.random()*10))
-        d=[a,b,c,"#","@","$","&"]
-        s+=random.choice(d)
-    print("\n\n","The generated password is:   ",s)
+
+def rand_pass_gen(n):
+    empty_string = ""
+    for i in range(1, n + 1):
+        a = chr(random.randint(65, 90))
+        b = chr(random.randint(97, 122))
+        c = str(int(random.random() * 10))
+        d = [a, b, c, "#", "@", "$", "&"]
+        empty_string += random.choice(d)
+    print("\n\n", "The generated password is:   ", empty_string)
+
+
+rand_pass_gen(var_length)
