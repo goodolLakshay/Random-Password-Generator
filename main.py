9b697078e64e7e18db9ba94a00ae6bb97b928ff2
Lakshay <lakshaychoudhary77712@gmail.com>
2022-11-14 14:26:28 +0530

    v2, better indication for input and output.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-n=int(input())
+n=int(input("Please enter the length of the password required: "))
+print("_____________________________________________________")
 s=""
 for i in range(1,n+1):
     a=chr(random.randint(65,90))
@@ -7,4 +8,4 @@ for i in range(1,n+1):
     c=str(int(random.random()*10))
     d=[a,b,c,"#","@","$","&"]
     s+=random.choice(d)
-print(s)
+print("\n\n","The generated password is:   ",s)
