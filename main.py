30a078828774aa719dd6488ec90ceb5c9e90a2ba
Lakshay <lakshaychoudhary77712@gmail.com>
2022-11-14 14:35:05 +0530

    added code to Pycharm-created_some_important_files

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 n=int(input("Please enter the length of the password required: "))
 print("_____________________________________________________")
-s=""
-for i in range(1,n+1):
-    a=chr(random.randint(65,90))
-    b=chr(random.randint(97,122))
-    c=str(int(random.random()*10))
-    d=[a,b,c,"#","@","$","&"]
-    s+=random.choice(d)
-print("\n\n","The generated password is:   ",s)
+
+def rand_pass_gen:  
+    s=""
+    for i in range(1,n+1):
+        a=chr(random.randint(65,90))
+        b=chr(random.randint(97,122))
+        c=str(int(random.random()*10))
+        d=[a,b,c,"#","@","$","&"]
+        s+=random.choice(d)
+    print("\n\n","The generated password is:   ",s)
